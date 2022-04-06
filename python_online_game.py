a650844fb59b7d10f44acb3355d711bb13cf435a
Jed Dryer <dryer.jed@gmail.com>
2022-04-06 11:08:32 -0500

    Adding client, network, and server

--- python_online_game.py
+++
@@ -1,4 +0,0 @@
-## 1.  Draw out the original hangman with ASCII for each turn and redraw it for every guess.  
-## 2. Use the file i/0 lesson to initially choose a  random word from a dictionary file online that you download and play against that word.
-## # https://www.techwithtim.net/tutorials/python-online-game-tutorial/
-
