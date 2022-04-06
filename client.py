5675a534f0eb9e3d837e5cbfd2ab256245ce565b
Jed Dryer <dryer.jed@gmail.com>
2022-04-06 11:22:27 -0500

    Updated Client, Server, and Network

--- client.py
+++ client.py
@@ -1,8 +1,5 @@
-## 1.  Draw out the original hangman with ASCII for each turn and redraw it for every guess.  
-## 2. Use the file i/0 lesson to initially choose a  random word from a dictionary file online that you download and play against that word.
-## # https://www.techwithtim.net/tutorials/python-online-game-tutorial/
-
 import pygame
+from network import Network
 
 width = 500
 height = 500
@@ -40,28 +37,49 @@ class Player():
         if keys[pygame.K_DOWN]:
             self.y += self.vel
 
+        self.update()
+
+    def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
 
 
-def redrawWindow(win,player):
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+def redrawWindow(win,player, player2):
     win.fill((255,255,255))
     player.draw(win)
+    player2.draw(win)
     pygame.display.update()
 
 
 def main():
     run = True
-    p = Player(50,50,100,100,(0,255,0))
+    n = Network()
+    startPos = read_pos(n.getPos())
+    p = Player(startPos[0],startPos[1],100,100,(0,255,0))
+    p2 = Player(0,0,100,100,(255,0,0))
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
+        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
+        p2.x = p2Pos[0]
+        p2.y = p2Pos[1]
+        p2.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
 
         p.move()
-        redrawWindow(win, p)
+        redrawWindow(win, p, p2)
 
 main()
